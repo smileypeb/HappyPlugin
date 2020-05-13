55114efb8e18df36e730175186df6205b345c61a
paulblanch <smileypeb@gmail.com>
2020-05-13 10:37:01 -0600

    Tracking ID: 1236 Description: Add target for ObjectiveC plugin

--- www/HappyPluginSwift.js
+++ www/HappyPluginSwift.js
@@ -1,13 +1,14 @@
 var exec = require('cordova/exec');
 
-var pluginName = "HappyPluginSwift"
+var pluginNameSwift = "HappyPluginSwift"
+var pluginNameObjectiveC = "HappyPlugin"
 
 var HappyPlugin = {
     happySwiftMethod: function(arg0, success, error) {
-      exec(success, error, "HappyPluginSwift", "happySwiftMethod", [arg0]);
+      exec(success, error, pluginNameSwift, "happySwiftMethod", [arg0]);
     },
     happyMethod: function(arg0, success, error) {
-        exec(success, error, "HappyPluginSwift", "happyMethod", [arg0]);
+        exec(success, error, pluginNameObjectiveC, "happyMethod", [arg0]);
     },
     happyMethodjs: function(arg0, success, error) {
         if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
